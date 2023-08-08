aa9ffe5fa545e8a6484d9c9cd271551b102ed366
Jie Hong, Lim <jiehong.lim@gmail.com>
2023-08-08 17:13:00 +0800

    added secrets template and clean up the code.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import pandas as pd
 from elasticsearch import Elasticsearch
 
 es_username = st.secrets["es_username"]
 es_password = st.secrets["es_password"]
 es_cloudid = st.secrets["es_cloudid"]
+es_index = st.secrets["es_index2"]
 
-es_index = "search-govtech-dev-docs"
-
-source_fields = ["title", "meta_description", "url"]
+source_fields = ["title", "headings", "url"]
 
 logo = "https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt5d10f3a91df97d15/620a9ac8849cd422f315b83d/logo-elastic-vertical-reverse.svg"
 
@@ -17,10 +15,19 @@ es = Elasticsearch(
     basic_auth=(es_username, es_password)
 )
 
+st.set_page_config(layout="wide")
+
 st.image(logo, width=100)
 
+search = st.text_input("Enter your semantic search", "how can I secure my network?")
 
-search = st.text_input("Enter your semantic search", "what products can I use to monitor my applications")
+bool_query = {
+    "bool": {
+        "must": [
+            {"match": {"body_content": search}},
+        ],
+    }
+},
 
 text_expand_query = {
     "text_expansion":{
@@ -32,65 +39,15 @@ text_expand_query = {
         }
     }
 
-
-resp = es.search(index=es_index, source=source_fields ,query=text_expand_query)
-print("Got %d Hits:" % resp['hits']['total']['value'])
+text_expand_resp = es.search(index=es_index, source=source_fields ,query=text_expand_query)
 
 st.divider()
 
-for hit in resp['hits']['hits']:
+
+st.write("Text Expansion Search")
+st.write("The text expansion query uses a natural language processing model to convert the query text into a list of token-weight pairs which are then used in a query against a rank features field.")
+for hit in text_expand_resp['hits']['hits']:
     st.write("%(title)s" % hit["_source"])
-    st.write("%(meta_description)s" % hit["_source"])
+    st.write("%(headings)s" % hit["_source"])
     st.write("%(url)s" % hit["_source"])
     st.divider()
-
-
-# querybody = {
-#     "query" : {
-#         "text_expansion":{
-#             "ml.inference.body_content_expanded.predicted_value":{
-
-#                 "model_id":".elser_model_1",
-#                 "model_text":"what products can I use to monitor my applications"
-#                 }
-#             }
-#         }
-# #     }
-
-# querybody = {
-#     "text_expansion":{
-#         "ml.inference.body_content_expanded.predicted_value":{
-
-#             "model_id":".elser_model_1",
-#             "model_text":"what products can I use to monitor my applications"
-#             }
-#         }
-#     }
-
-# resp = es.search(index="search-govtech-dev-docs", query=querybody)
-
-# total_hits = resp['aggregations']['match_count']['value']
-
-# st.write(total_hits)
-
-# st.write("DB password:", st.secrets["es_password"])
-
-
-# resp = es.search(index="test-index", query={"match_all": {}})
-# print("Got %d Hits:" % resp['hits']['total']['value'])
-# for hit in resp['hits']['hits']:
-#     print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
-
-
-# GET search-govtech-dev-docs/_search
-# {
-#    "_source" : ["title", "meta_description"],
-#    "query":{
-#       "text_expansion":{
-#          "ml.inference.body_content_expanded.predicted_value":{
-#             "model_id":".elser_model_1",
-#             "model_text":"what products can I use to monitor my applications"
-#          }
-#       }
-#    }
-# }
